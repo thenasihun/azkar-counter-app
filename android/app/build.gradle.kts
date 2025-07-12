62a21a3cfd732983a5a29cb31c05a735fb9dac65
thenasihun <thenasihun@gmail.com>
2025-07-12 09:43:52 +0500

    Version 1.3.0 Preset & Categories Added & Published

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.nasihun.azkar_counter"
         minSdk = 21
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
     }
 
     buildTypes {
